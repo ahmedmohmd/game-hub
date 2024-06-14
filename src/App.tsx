a6d2ce9bcc51341dfc44550052aabfd2698847c3
ahmedmohmd <er.ahmed.3301@gmail.com>
2024-06-14 19:00:33 +0300

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css';
-
 function App() {
   return (
-    <div>
-      Hello, World
-      <img src="/..." />
+    <div className="flex flex-col text-white">
+      <div className="flex-1 bg-blue-500">nav</div>
+      <div className="flex flex-row">
+        <div className="aside lg:block hidden w-1/3 bg-yellow-500">aside</div>
+        <div className="flex-1 bg-red-500">main</div>
+      </div>
     </div>
   );
 }
